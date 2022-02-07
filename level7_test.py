c20b37a60dada33cb65e0c86b6732340f97a4cb8
medmax33 <m.a.medvedev@outlook.com>
2022-02-07 18:57:18 +0500

    add new level7_test.py

--- level7_test.py
+++ level7_test.py
@@ -6,6 +6,8 @@ from level7_v1 import WordSearch
 class MyTests(unittest.TestCase):
 
     phrase_1 = '1) строка разбивается на набор строк через выравнивание по заданной ширине.'
+    phrase_2 = 'Метод format() – самый гибкий и полезный метод форматирования строк. Фигурные скобки {} ' \
+               'используются в качестве заполнителя в строке'
 
     def test_in_example(self):
         self.assertEqual(WordSearch(12, self.phrase_1, 'строк'), [0, 0, 0, 1, 0, 0, 0])
@@ -15,6 +17,9 @@ class MyTests(unittest.TestCase):
 
     def test_3_12345(self):
         self.assertEqual(WordSearch(3, '12345', '123'), [0, 0])
+
+    def test_long(self):
+        self.assertEqual(WordSearch(15, self.phrase_2, 'форматирования'), [0, 0, 0, 1, 0, 0, 0, 0, 0, 0])
 
 
 if __name__ == '__main__':
