a2acc7c24c78ccdde05c2ba40ae0f4c1b70e1077
medmax33 <m.a.medvedev@outlook.com>
2022-02-20 20:01:24 +0500

    add new level11.py

--- level11.py
+++ level11.py
@@ -1,2 +1,18 @@
 def BigMinus(s1: str, s2: str) -> str:
-    return str(abs(int(s1) - int(s2)))
+    if len(s2) > len(s1):
+        s1, s2 = s2, s1
+    index = len(s1)
+    s2 = s2.rjust(index, '0')
+
+    result = ''
+    ostatok = 0
+    for i in range(index - 1, -1, -1):
+        if int(s1[i]) < int(s2[i]):
+            digit = 10 + int(s1[i]) - int(s2[i]) - ostatok
+            ostatok = 1
+        else:
+            digit = int(s1[i]) - int(s2[i]) - ostatok
+            ostatok = 0
+        result = str(digit) + result
+
+    return result
