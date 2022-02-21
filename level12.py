329c5b38e63d95484a22477baef715322477ddbb
medmax33 <m.a.medvedev@outlook.com>
2022-02-21 17:50:16 +0500

    add level12.py

---
+++ level12.py
@@ -0,0 +1,36 @@
+def MassVote(n: int, votes: list) -> str:
+    # check if n equal len votes
+    if n != len(votes):
+        return 'n doesn\'t fit length of votes'
+
+    # try to calculate total votes
+    try:
+        total_votes = 0
+        for _ in votes:
+            total_votes += _
+    except TypeError:
+        return 'check data in votes'
+
+    # calculate list of percentage of each candidate
+    percentage = []
+    for _ in votes:
+        percentage.append(round((_ / total_votes) * 100, 3))
+
+    # make sorted list of percentage
+    percent: list = sorted(percentage, reverse=True)
+
+    # we have a winner?
+    if percent[0] == percent[1]:
+        return 'no winner'
+
+    # find number of winner
+    k: str = ''
+    for i in range(n):
+        if max(percentage) == percentage[i]:
+            k = str(i + 1)
+
+    # majority or minority?
+    if percent[0] > 50:
+        return 'majority winner ' + k
+    else:
+        return 'minority winner ' + k
