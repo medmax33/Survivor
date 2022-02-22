3e624008a7c23f7236085885e9ef702c3d071c12
medmax33 <m.a.medvedev@outlook.com>
2022-02-22 14:52:29 +0500

    add level13.py

---
+++ level13.py
@@ -0,0 +1,17 @@
+def UFO(n: int, data: list, octal: bool) -> list:
+    try:
+        # check data length
+        if n != len(data):
+            return [0]
+
+        result = []
+        for _ in data:
+            if octal:
+                result.append(int(str(_), 8))
+            else:
+                result.append(int(str(_), 16))
+
+        return result
+
+    except TypeError:
+        return [0]
