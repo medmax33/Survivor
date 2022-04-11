ad2410806f494809542f9090aaaa9a5a9253695c
medmax33 <m.a.medvedev@outlook.com>
2022-04-11 22:25:59 +0500

    add level25.py

---
+++ level25.py
@@ -0,0 +1,23 @@
+def TransformTransform(a: list, n: int) -> bool:
+
+    if n != len(a):
+        return False
+    print(Transform(Transform(a)))
+    print(sum(Transform(Transform(a))))
+
+    keykey = sum(Transform(Transform(a)))
+
+    if keykey % 2 == 0:
+        return True
+    return False
+
+
+def Transform(a: list) -> list:
+    b: list = []
+
+    for i in range(len(a)):
+        for j in range(len(a) - i):
+            k = i + j
+            b.append(max(a[j:k + 1]))
+
+    return b
