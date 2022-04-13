30a546080daeca05a6b2d2a5036565be952ad1ec
medmax33 <m.a.medvedev@outlook.com>
2022-04-13 16:03:37 +0500

    add level27.py

---
+++ level27.py
@@ -0,0 +1,27 @@
+def Football(f: list, n: int) -> bool:
+    if f:
+        k: list = []
+        m: list = []
+        f_up: list = f.copy()
+        f_up.sort()
+
+        for i in range(n):
+            if f[i] != f_up[i] and f[i] > f[i + (1 if i < n-1 else 0)]:
+                k.append(i)
+                m.append(1)
+            elif f[i] != f_up[i]:
+                k.append(i)
+                m.append(0)
+            else:
+                m.append(0)
+
+        if len(k) == 0:
+            return True
+        elif len(k) == 2:
+            return True
+        elif len(k) > 2 and 0 not in m[k[0]:k[-1]]:
+            return True
+        else:
+            return False
+
+    return False
